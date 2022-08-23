ed4bda14b589068e246a1fe0a63a393bce49d62e
Braden <braden.allmond@gmail.com>
2022-08-23 09:47:53 +0200

    added tauDirectory to use and output directory as arguments to png printer

--- savePNGsFromRoot.py
+++ savePNGsFromRoot.py
@@ -1,5 +1,7 @@
 import ROOT 
 import argparse 
+import sys
+import os
  
 ROOT.gROOT.SetBatch(True) # sets visual display off (i.e. no graphs/TCanvas)
 
@@ -8,9 +10,19 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='Open a .root file') 
   parser.add_argument('-i', '--inputRootFile', dest='inFilename', action='store', 
                     help='the input .root file\'s name') 
+  parser.add_argument('-t', '--tauDirectory', dest='tauDirectory', action='store', default='TagAndProbe',
+                    help='which tau directory do you want to save images from?')
+  parser.add_argument('-o', '--outputDirectoyr', dest='outputDirectory', action='store', default='images',
+                    help='name a directory to store the output.')
   args = parser.parse_args() 
 
   inFile = ROOT.TFile.Open(args.inFilename,"READ") 
+
+  possibleTauDirs = ["Inclusive", "PFTaus", "TagAndProbe"]
+  if (args.tauDirectory not in possibleTauDirs):
+    print("Not a directory. Choose one of these: {}".format(possibleTauDirs))
+    sys.exit()
+
 
   #   ^^^^ getting a file
   #   vvvv PyROOT file stuff
@@ -26,15 +38,14 @@ if __name__ == "__main__":
   # on the end of a directory (or tree) (like dir.GetListOfKeys().Print())
   # When executed, this lists all the names of the objects present.
 
-  # Don't end your final directory with /
+  # DON'T end your final directory with "/"
   # It causes a null-pointer for some reason
 
-  directory = "DQMData/Run 357612/HLT/Run summary/TAU/TagAndProbe"
+  directory = "DQMData/Run 357612/HLT/Run summary/TAU/" + args.tauDirectory
   listOfHLTs = inFile.Get(directory).GetListOfKeys()
-  # to print your keys, use the Print() function on the object
-  #inFile.Get(directory).GetListOfKeys().Print()
 
-  # generate same graphs for every HLT path in TagAndProbe  
+  os.mkdir(args.outputDirectory)
+  # generate same graphs for every HLT path in given tauDirectory
   for HLT in listOfHLTs:
     pathToHLT = directory + "/" + HLT.GetName()
     print(pathToHLT)
@@ -50,7 +61,7 @@ if __name__ == "__main__":
       c1 = ROOT.TCanvas("c1", "", 600, 400)
       inFile.Get(pathToGraph).Draw()
       # canvas holds whatever you drew last, Printing a canvas saves it
-      c1.Print(graphFilename, "png")
+      c1.Print(args.outputDirectory+'/'+graphFilename, "png")
 
 
 
